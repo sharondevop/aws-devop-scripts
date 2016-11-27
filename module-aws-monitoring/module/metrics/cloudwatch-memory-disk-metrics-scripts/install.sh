ac37357e7c057c19c7779c9bad71538b8db72df4
Sharon Mafgaoker <sharon@cloud5.co.il>
2016-11-27 17:25:50 +0200

    add # to bin/bash

--- module-aws-monitoring/module/metrics/cloudwatch-memory-disk-metrics-scripts/install.sh
+++ module-aws-monitoring/module/metrics/cloudwatch-memory-disk-metrics-scripts/install.sh
@@ -1,4 +1,3 @@
-!/bin/bash
 ################################################################################                                                                            
 # Send memory usage and disk metrics to Amazon CloudWatch
 #  
@@ -9,6 +8,7 @@
 # You are free to use, modify and redistribute this software in any form
 # under the conditions described in the LICENSE file included.
 ################################################################################
+#!/bin/bash
 
 # select the local disk on wich to report to Cloudwatch, we pass the mount point as parameters.
 DISKPATH=$(df -l --type={xfs,ext4} | grep ^/dev | awk '{print "--disk-path="$6 }' | paste -sd ' ')
