5e24f4dcbda6e13304e95844130551c943d3c1ea
Sharon Mafgaoker <sharon@cloud5.co.il>
2016-12-19 00:54:43 +0200

    fix Patterns replace in /etc/bashrc

--- host-name.sh
+++ host-name.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
-# Replace PS1 /h with $NICKNAME
+# Replace PS1 /h in /etc/bashrc with $NICKNAME
 
-# Patterns to find in the file
-pattern='&&\sPS1="\[.u@.h\s'
+# Patterns to find in the file '&&\sPS1="\[.u@.h\s'
 
-# Pattern to be replaced
-replace="\$NICKNAME"
-echo "$replace"
-grep --color  "$pattern" /etc/bashrc
+# Check if need to replace the file.
+if grep -q '&&\sPS1="\[.u@.$NICKNAME\s' /etc/bashrc ; then
+   echo "\$NICKNAME  In place"
+   exit 0
+fi
 
-ed  /home/ec2-user/bashrch_copy <<< '/\$pattern/s/h/t/'
+#copy the original file  /etc/bashrc
+if [ -e /etc/bashrc ]; then
+        sudo cp /etc/bashrc /etc/bashrc.bak >&2
+        #Substitutions using ed
+        sudo ed -s /etc/bashrc  <<< $'H\n/&&\sPS1="\[.u@.h\s/s/h/$NICKNAME/\n,w' > /dev/null 2>&1
+else
+     echo ">>> ERROR /etc/bashrc file not found or you don't have permission to edit the file"
+     exit 1
+fi
+
+# Setup hostname
+#currhost=$(cat /etc/hostname)
+#echo "Enter a hostname for this pi: "
+#read newhost
+#sudo sed -i "s/$currhost/$newhost/g" /etc/{hosts,hostname}
